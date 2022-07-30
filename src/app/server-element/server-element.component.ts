69f75830fc3695390629362a2d9815cf183c7386
keepprogress <keepprogress@gmail.com>
2022-07-30 20:01:04 +0800

    section5: 79 Seeing Lifecycle Hooks in Action

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,14 @@
-import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  ViewEncapsulation,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -9,13 +19,54 @@ import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
   // Native is use shadowdownloading to load the styles. but only when the browser supports it.
   encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
   // adding the @Input() decorator to the name property will make it available to the parent component
   @Input() element: {type: string, name: string, content: string};
+  @Input() name: string;
 
-  constructor() { }
+  constructor() {
+    console.log('constructor called');
+  }
+
+  ngOnDestroy(): void {
+        console.log('ngOnDestroy called');
+    }
+
+  ngAfterViewChecked(): void {
+        console.log('ngAfterViewChecked called');
+    }
+
+  ngAfterViewInit(): void {
+        console.log('ngAfterViewInit called');
+    }
+
+  ngAfterContentInit(): void {
+        console.log('ngAfterContentInit called');
+    }
+
+  ngDoCheck(): void {
+        console.log('ngDoCheck called');
+    }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges called');
+    console.log(changes);
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit called');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked called');
   }
 
 }
